e6a8f76a40f771ab6d35a222ae6e77bdc96d028c
Moussa Hassana <moussahassana42@gmail.com>
2025-05-23 08:49:19 +0100

    feat(decorators): implement transactional decorator for DB transaction management

---
+++ python-decorators-0x01/2-transactional.py
@@ -0,0 +1,24 @@
+import sqlite3 
+import functools
+with_db_connection=__import__('1-with_db_connection').with_db_connection
+
+def transactional(func):
+    @functools.wraps(func)
+    def wrapper(conn, *args, **kwargs):
+        try:
+            result = func(conn, *args, **kwargs)
+            conn.commit()
+            return result
+        except Exception as e:
+            conn.rollback()
+            raise e
+    return wrapper
+
+@with_db_connection 
+@transactional 
+def update_user_email(conn, user_id, new_email): 
+    cursor = conn.cursor() 
+    cursor.execute("UPDATE users SET email = ? WHERE id = ?", (new_email, user_id)) 
+#### Update user's email with automatic transaction handling 
+
+update_user_email(user_id=1, new_email='Crawford_Cartwright@hotmail.com')
