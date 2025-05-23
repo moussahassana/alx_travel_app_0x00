f6c1571e610f7ace5150925c7e7aaac06de60dcc
Moussa Hassana <moussahassana42@gmail.com>
2025-05-23 16:45:43 +0100

    feat(decorators): implement retry_on_failure decorator for resilient DB queries

---
+++ python-decorators-0x01/3-retry_on_failure.py
@@ -0,0 +1,31 @@
+import time
+import sqlite3 
+import functools
+with_db_connection=__import__("1-with_db_connection").with_db_connection
+
+# This decorator will retry the function if it raises an exception
+def retry_on_failure(retries, delay):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            for attempt in range(retries):
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    print(f"Attempt {attempt + 1} failed: {e}")
+                    time.sleep(delay)
+            raise Exception("All retry attempts failed.")
+        return wrapper
+    return decorator
+
+@with_db_connection
+@retry_on_failure(retries=3, delay=1)
+def fetch_users_with_retry(conn):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users")
+    return cursor.fetchall()
+
+#### attempt to fetch users with automatic retry on failure
+
+users = fetch_users_with_retry()
+print(users)
